3b2a010613deddd5694d6a4b3ab60329fc8d0f94
Matt Styles <matt@personalurban.com>
2018-01-08 18:18:33 +0000

    add navigator wraps children in transition components

--- packages/radios/stories/pageNavigation.stories.js
+++ packages/radios/stories/pageNavigation.stories.js
@@ -3,16 +3,13 @@ import React, {Component} from 'react'
 import {storiesOf} from '@storybook/react'
 import createHistory from 'history/createMemoryHistory'
 import oc from 'open-color'
-import styled from 'styled-components'
 import {Signal} from 'raid'
 
 import {Navigator, createActions} from '../src/navigator/index.js'
 
 import {PageTransition, PageGroup, childFactory, TRANSITIONS} from '../src/transitions/page'
-import {AnimateGroup, PageIn, PageOut, Fade, H1, Button, View} from '../src'
+import {H1, Button, View} from '../src'
 import {AppHeader} from '../storybook/app'
-
-import {TransitionGroup, Transition} from 'react-transition-group'
 
 const signal = Signal.of()
 const history = createHistory()
@@ -34,101 +31,26 @@ signal.register((state, event) => {
   return state
 })
 
-const Group = styled(TransitionGroup)`
-  width: 100%;
-  height: 100%;
-  position: relative;
-`
-
-const DURATION = 200
-const Trans = ({
-  key,
-  route,
-  children,
-  transition,
-  ...props
-}) => (
-  <Transition
-    {...props}
-    key={key}
-    timeout={{
-      enter: DURATION,
-      exit: DURATION * 1.5
-    }}
-    unmountOnExit
-    appear
-    onEnter={(node) => {
-      console.log('onEnter', transition, route)
-      node.style.transition = `all ${DURATION}ms linear`
-      node.style.position = 'absolute'
-      node.style.top = '0px'
-      node.style.left = '0px'
-      node.style.bottom = '0px'
-      node.style.right = '0px'
-
-      if (!transition) {
-        return
-      }
-
-      if (transition === 'PageIn') {
-        node.style.zIndex = '10'
-        node.style.transform = 'translateX(100%)'
-        return
-      }
-
-      if (transition === 'PageOut') {
-        node.style.zIndex = '5'
-        node.style.transform = 'translateX(-25%)'
-      }
-    }}
-    onEntering={node => {
-      console.log('onEntering', transition, route)
-      // Defer to ensure that transition occurs between onEnter
-      // and onEntering. This ensures that there is not a gap between
-      // exiting and entering children
-      setTimeout(() => {
-        node.style.transform = 'translateX(0%)'
-      }, 10)
-    }}
-    onEntered={(node) => {
-      console.log('onEntered', transition, route)
-      // node.style.transform = 'translateX(0%)'
-    }}
-    onExit={node => {
-      console.log('onExit', transition, route)
-    }}
-    onExiting={node => {
-      console.log('onExiting', transition, route)
-      if (!transition) {
-        return
-      }
-
-      if (transition === 'PageIn') {
-        node.style.zIndex = '5'
-        node.style.transform = 'translateX(-25%)'
-        return
-      }
-
-      if (transition === 'PageOut') {
-        node.style.zIndex = '10'
-        node.style.transform = 'translateX(100%)'
-      }
-    }}
-    onExited={node => {
-      console.log('onExited', transition, route)
-    }}
-  >
-    {children}
-  </Transition>
-)
-
-const childFactoryCreator = transition => child => (
-  React.cloneElement(child, {
-    transition
-  })
-)
-
 class NavigationTransition extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      navigation: []
+    }
+
+    this.mapChildren = child => {
+      return (
+        <PageTransition
+          key={child.key}
+          route={child.props.route}
+        >
+          {child}
+        </PageTransition>
+      )
+    }
+  }
+
   componentDidMount () {
     this.dispose = signal.observe(state => {
       this.setState(s => state)
@@ -137,10 +59,6 @@ class NavigationTransition extends Component {
 
   componentWillUnmount () {
     if (this.dispose) this.dispose()
-  }
-
-  state = {
-    navigation: []
   }
 
   render () {
@@ -157,8 +75,9 @@ class NavigationTransition extends Component {
           childFactory: childFactory(transition)
         }}
       >
+        {React.Children.map(this.props.children, this.mapChildren)}
         {/* {this.props.children} */}
-        <PageTransition
+        {/* <PageTransition
           key='root'
           route='/'
           transition={transition}
@@ -171,7 +90,7 @@ class NavigationTransition extends Component {
           transition={transition}
         >
           <ChildView />
-        </PageTransition>
+        </PageTransition> */}
       </Navigator>
     )
   }
@@ -180,7 +99,15 @@ class NavigationTransition extends Component {
 const RootView = () => (
   <View data-name='root' isPadded style={{background: oc.gray[3], height: '100%', boxSizing: 'border-box'}}>
     <H1>Root</H1>
+    <Button primary onClick={e => actions.push('/middle')}>Settings</Button>
+  </View>
+)
+
+const MiddleView = () => (
+  <View data-name='middle' isPadded style={{background: oc.red[2], height: '100%', boxSizing: 'border-box'}}>
+    <H1>Middle</H1>
     <Button primary onClick={e => actions.push('/child')}>Settings</Button>
+    <Button primary onClick={e => actions.back()}>Back</Button>
   </View>
 )
 
@@ -196,10 +123,21 @@ storiesOf('navigator', module)
     <View flex>
       <AppHeader title='Page fade transition' />
       <NavigationTransition>
-        {/* <Transition key='root' route='/'><RootView /></Transition>
-        <Transition key='child' route='/child'><ChildView /></Transition> */}
-        {/* <PageIn key='root' route='/'><RootView /></PageIn>
-        <PageIn key='child' route='/child'><ChildView /></PageIn> */}
+        {/* <PageTransition
+          key='root'
+          route='/'
+        >
+          <RootView />
+        </PageTransition>
+        <PageTransition
+          key='child'
+          route='/child'
+        >
+          <ChildView />
+        </PageTransition> */}
+        <RootView route='/' key='root' />
+        <MiddleView route='/middle' key='middle' />
+        <ChildView route='/child' key='child' />
       </NavigationTransition>
     </View>
   ))
